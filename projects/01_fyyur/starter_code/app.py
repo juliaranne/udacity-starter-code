26d2900fe568c17f152583fe200c392a8baa12a0
julia-anne <julia.robinson@spareroom.co.uk>
2025-06-25 17:20:59 +0100

    artist post form

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -261,30 +261,35 @@ def create_venue_form():
 def create_venue_submission():
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
-  name = request.form['name']
-  city = request.form['city']
-  state = request.form['state']
-  address = request.form['address']
-  phone = request.form['phone']
-  image_link = request.form['image_link']
-  facebook_link = request.form['facebook_link']
-  website_link = request.form['website_link']
-  seeking_talent = True if request.form['seeking_talent'] == 'y' else False
-  seeking_description = request.form['seeking_description']
-  genre_names = request.form.getlist('genres')
+  try:
+    name = request.form['name']
+    city = request.form['city']
+    state = request.form['state']
+    address = request.form['address']
+    phone = request.form['phone']
+    image_link = request.form['image_link']
+    facebook_link = request.form['facebook_link']
+    website_link = request.form['website_link']
+    seeking_talent = True if request.form['seeking_talent'] == 'y' else False
+    seeking_description = request.form['seeking_description']
+    genre_names = request.form.getlist('genres')
 
-  genre_objects = []
-  for genre_name in genre_names:
-    genre = Genres.query.filter_by(genre=genre_name).first()
-    genre_objects.append(genre)
+    genre_objects = []
+    for genre_name in genre_names:
+      genre = Genres.query.filter_by(genre=genre_name).first()
+      genre_objects.append(genre)
 
-  new_venue = Venue(name=name, city=city, state=state, address=address, phone=phone, genres=genre_objects, image_link=image_link, facebook_link=facebook_link, website_link=website_link, seeking_talent=seeking_talent, seeking_description=seeking_description)
-  db.session.add(new_venue)
-  db.session.commit()
-  db.session.close()
+    new_venue = Venue(name=name, city=city, state=state, address=address, phone=phone, genres=genre_objects, image_link=image_link, facebook_link=facebook_link, website_link=website_link, seeking_talent=seeking_talent, seeking_description=seeking_description)
+    db.session.add(new_venue)
+    db.session.commit()
+    flash('Venue ' + request.form['name'] + ' was successfully listed!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. Venue ' + request.form['name'] + ' could not be listed.')
+  finally:
+    db.session.close()
 
   # on successful db insert, flash success
-  flash('Venue ' + request.form['name'] + ' was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
@@ -476,9 +481,34 @@ def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
+  try:
+    name = request.form['name']
+    city = request.form['city']
+    state = request.form['state']
+    phone = request.form['phone']
+    image_link = request.form['image_link']
+    facebook_link = request.form['facebook_link']
+    website_link = request.form['website_link']
+    seeking_venue = True if request.form['seeking_venue'] == 'y' else False
+    seeking_description = request.form['seeking_description']
+    genre_names = request.form.getlist('genres')
+
+    genre_objects = []
+    for genre_name in genre_names:
+      genre = Genres.query.filter_by(genre=genre_name).first()
+      genre_objects.append(genre)
+
+    new_venue = Venue(name=name, city=city, state=state, phone=phone, genres=genre_objects, image_link=image_link, facebook_link=facebook_link, website_link=website_link, seeking_venue=seeking_venue, seeking_description=seeking_description)
+    db.session.add(new_venue)
+    db.session.commit()
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.')
+  finally:
+    db.session.close()
 
   # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
   return render_template('pages/home.html')
